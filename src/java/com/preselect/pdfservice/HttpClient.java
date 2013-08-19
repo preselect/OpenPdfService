453d49af82e11aca5cf0f796328f2ba8df53b89e
Geschan <git@misc.geschan.de>
2013-08-19 11:58:45 +0200

    Rename package

--- src/java/com/preselect/pdfservice/HttpClient.java
+++ src/java/com/preselect/pdfservice/HttpClient.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package PdfResource;
+package com.preselect.pdfservice;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
