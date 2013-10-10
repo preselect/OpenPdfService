1913bfc381733307ba9f2f1fab3b7428751f612a
Moritz Munte <git@misc.geschan.de>
2013-10-10 13:24:44 +0200

    Added status class

---
+++ src/java/com/preselect/pdfservice/Status.java
@@ -0,0 +1,8 @@
+package com.preselect.pdfservice;
+
+public class Status {
+
+        public static final String ERROR = "ERROR";
+        public static final String IN_PROGRESS = "INPROGRESS";
+        public static final String COMPLETED = "COMPLETED";
+}
